c9dc546e9157f32104422e70617ed1577074b293
lilightc <74816995+lilightc@users.noreply.github.com>
2024-12-03 15:22:07 -0800

    Update robot.py

--- Software/Devices/robot.py
+++ Software/Devices/robot.py
@@ -2,24 +2,24 @@
 
 # Class comment
 class Robot:
-    def __init__(self, id = 0, position = (0, 0), orientation = (0, 0), power_level = 100, motor = None, sensors = None, collision_indicator = None,
-                  power_harvest = None, role = "non-leader", transmitted_data = None, uwb_localization = None):
+    def __init__(self, id = 0, position = (0, 0), orientation = (0, 0), power_level = 100, role = "non-leader", data_receiver = None, executor = None,
+                  sensors = None, power_harvester = None, data_transmitter = None, uwb_transmitter = None):
         self.id = id
         self.position = position
         self.orientation = orientation
         self.power_level = power_level
-        self.motor = motor
-        self.sensors = sensors
-        self.collision_indicator = collision_indicator
-        self.power_harvest = power_harvest
         self.role = role
-        self.data_transmitter = transmitted_data
-        self.uwb_transmitter = uwb_localization
+        self.data_receiver = data_receiver
+        self.executor = executor
+        self.sensors = sensors
+        self.power_harvester = power_harvester
+        self.data_transmitter = data_transmitter
+        self.uwb_transmitter = uwb_transmitter
 
     def update(self):
-        self.motor.update()
+        self.executor.update()
         self.sensors.update()
-        self.collision_indicator.update()
-        self.power_harvest.update()
+        self.power_harvester.update()
         self.data_transmitter.update()
         self.uwb_transmitter.update()
+        self.data_receiver.update()
