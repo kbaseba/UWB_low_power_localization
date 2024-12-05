cbb0ad6ec628f017918381bae5ae5639bacb4018
Tilboon Elberier <67396010+kbaseba@users.noreply.github.com>
2024-12-05 15:34:48 -0800

    adding further code

--- Software/Central_Hub/central_hub.py
+++ Software/Central_Hub/central_hub.py
@@ -1,9 +1,85 @@
 # Imports
 
+from .Sector_Assignment.sector_assignment import SectorAssignment
+from .Leader_Selection.leader_selection import LeaderSelection
+from .data_reception import DataReception
+from .Estimator.estimator import Estimator
+from .mapping import Mapping
+from .Swarm_Coordination.swarm_coordination import SwarmCoordination
+
 # Class comment
 class CentralHub:
-    def __init__(self):
-        pass
+    def __init__(self, map, num_sectors, total_num_sensor_nodes, node_range):
+        self.map = map
+        self.leader_nodes = None
+        self.sector_assignment = SectorAssignment(self.map.width, self.map.height, num_sectors, total_num_sensor_nodes, node_range, self.map.obstacles)
+        self.leader_selection = LeaderSelection()
+        self.sectors, self.hub = self.sector_assignment.update()
 
+        self.data_reception = DataReception()
+        self.estimator = Estimator()
+        self.mapping = Mapping()
+        self.swarm_coordination = SwarmCoordination()
+        
     def update(self):
-        pass
+        self.leader_nodes = self.leader_selection.update(self.hub.robots)
+
+        for robot in self.hub.robots:
+            robot.move()
+
+        for robot in self.hub.robots:
+            robot.update()
+
+        self.data_reception.update(self.hub.robots)
+        self.estimator.update(self.hub.robots)
+        self.mapping.update(self.hub.robots)
+        self.swarm_coordination.update(self.hub.robots)
+
+
+        
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # Extract robot positions
+        sensor_node_positions = [robot.position for robot in self.hub.robots]
+
+        # Extract anchor positions
+        anchor_position = [anchor.position for anchor in self.hub.anchors]
+
+        # Extract hub position
+        hub_position = self.hub.position
+
+        # Step 3: Leader selection
+        leader_selection = LeaderSelection()
+        leader_nodes = leader_selection.update(self.hub.robots)
+
+        # Step 5: Print leader nodes with their IDs and power levels
+        print("Leader Nodes:")
+        for sector, leader_id in leader_nodes.items():
+            if leader_id is not None:
+                # Find the corresponding robot object
+                leader_robot = next(robot for robot in self.hub.robots if robot.id == leader_id)
+                print(f"Sector: {sector}, Leader ID: {leader_robot.id}, Power Level: {leader_robot.power_level}")
+            else:
+                print(f"Sector: {sector}, No leader assigned")
+
+        self.map.plot_map(sectors=self.sectors, sensor_node_positions=sensor_node_positions, anchor_positions=anchor_position, hub_position=hub_position)
+
+
--- Software/simulator.py
+++ Software/simulator.py
@@ -1,87 +1,29 @@
-# Imports
-from Devices.hub import Hub
-from Devices.anchor import Anchor
-from Devices.robot import Robot
+import numpy as np
+
+from testbesnch_tools import simulation_configuration_setup
+
 from map_creation import Map
-import random, time
+from Central_Hub.central_hub import CentralHub
+from Sensor_Nodes.sensor_nodes import SensorNodes
 
 # Class comment
-"""
-The simulator will
-1. Generate the map
-2. Generate the anchors and place them at vertices of the map
-3. Generate the robots and place them randomly throughout the map (need to implement obstacle avoidance)
-4. Update the hub, then update the robots
-"""
 class Simulator:
-    def __init__(self, num_robots=4, map_dimensions = [100,100]):
-        #Generate the map
-        self.map_dimensions = map_dimensions
-        self.map = Map(width=self.map_dimensions[0], height=self.map_dimensions[1], num_obstacles=5, light_variation=True)
-        
-        #Generating anchors and placing at 3 vertices of map
-        self.anchors = []
-        vertices = [(0,0),(0,map_dimensions[0]),(map_dimensions[1],0)]
-        for vertex in vertices:
-            anchor = Anchor(position=vertex)
-            self.addAnchor(anchor)
-
-        #Generating robots at random positions in map that don't intersect obstacles
-        self.robots = []
-        for i in range(num_robots):
-            #Randomly choosing a position, making sure that it doesn't intersect any obstacles
-            pos = (random.randint(0,self.map_dimensions[0]),random.randint(0,self.map_dimensions[1]))
-            while self.map._check_overlap("circle", pos[0],pos[1],0.5):
-                print('Position intersects obstacle, retrying...')
-                pos = (random.randint(0,self.map_dimensions[0]),random.randint(0,self.map_dimensions[1]))
-            robot = Robot(id=i, position=pos)
-            robot.update(map=self.map)
-            self.addRobot(robot)
-
-        #Generating hub
-        self.Hub = Hub(self.anchors, self.robots)
-        
-        #Plotting the map
-        self.map.plot_map(self.robots)
-
-        pass
-    
-    #Function to add anchors to simulation
-    def addAnchor(self, anchor):
-        self.anchors.append(anchor)
-        pass
-    #Function to add robots to simulation
-    def addRobot(self,robot):
-        self.robots.append(robot)
-        pass
-
-    #Status to check simulator
-    def printStatus(self):
-        #Printing anchors
-        for anchor in self.anchors:
-            print(f"Anchor position: {anchor.position}")
-        #Printing robot positions
-        for robot in self.robots:
-            print(f"Robot ID: {robot.id}")
-            print(f"\tPosition: {robot.position}")
+    def __init__(self, map_width, map_height, num_obstacles, light_variation, num_sectors, total_num_sensor_nodes, node_range):
+        self.map = Map(width=map_width, height=map_height, num_obstacles=num_obstacles, light_variation=light_variation)
+        self.central_hub = CentralHub(self.map, num_sectors, total_num_sensor_nodes, node_range)
+        # self.sensor_nodes = SensorNodes()
 
     def update(self):
-        #Update Hub
-        self.Hub.update()
-        #Update robots
-        for robot in self.robots:
-            robot.update(self.map)
-        pass
+        while True:
+            self.central_hub.update()
 
-if __name__ =='__main__':
-    #Creating simulator instance
-    simulator = Simulator()
-    simulator.printStatus()
-    #Initializing time-step
-    t = 0
-    time_unit = 0.5
-    while True:
-        simulator.update()
-        time.sleep(time_unit)
-        t += 1
-        print(f"Time step: {t}, time: {time_unit*t}\t")
+if __name__ == "__main__":
+    # Load configuration from JSON file
+    map_width, map_height, num_obstacles, light_variation, num_sectors, total_num_sensor_nodes, node_range, random_seed = simulation_configuration_setup()
+
+    # Set random seed for reproducibility
+    np.random.seed(random_seed)
+
+    simulator = Simulator(map_width, map_height, num_obstacles, light_variation, num_sectors, total_num_sensor_nodes, node_range)
+
+    simulator.update()
