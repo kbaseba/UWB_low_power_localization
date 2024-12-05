d4c1759d4966f1074ddc175dbcf07e694d28bb86
colinrec34 <colinrecker@gmail.com>
2024-12-04 17:36:03 -0800

    building out robot, hub, and anchors

--- Software/Devices/hub.py
+++ Software/Devices/hub.py
@@ -5,23 +5,27 @@ class Hub:
     def __init__(self, robots=[], anchors=[]):
         self.robots = robots
         self.anchors = anchors
+    def __init__(self, robots=[], anchors=[]):
+        self.robots = robots
+        self.anchors = anchors
         pass
 
     def update(self):
-        #Assign leader to robot with highest power
-        leaderID= 
-        for robot in self.robots:
-
-
         #Update the three anchors, localizing leader robot
         anchorMeasurements = []
         for anchor in self.anchors:
             anchorMeasure = anchor.update(self.robots)
             anchorMeasurements.append(anchorMeasure)
-        xpos,ypos = 0,0
-        for position in anchorMeasurements:
-            xpos += position[0]
-            ypos += position[1]
-        avgMeasuredDist = (xpos/num_anchorsMeasurements, ypos/num_anchorsMeasurements)
-        print(avgMeasuredDist)
-        return avgMeasuredDist
+        #If no robots are assigned leader
+        num_anchorsMeasurements = len(anchorMeasurements)
+        if num_anchorsMeasurements==0:
+            #print('No leader robots')
+            pass
+        else:
+            xpos,ypos = 0,0
+            for position in anchorMeasurements:
+                xpos += position[0]
+                ypos += position[1]
+            avgMeasuredDist = (xpos/num_anchorsMeasurements, ypos/num_anchorsMeasurements)
+            print(avgMeasuredDist)
+            return avgMeasuredDist
